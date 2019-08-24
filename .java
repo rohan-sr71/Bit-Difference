67a602eb37151ca927a04708f16a7b53d66042a3
rohan-sr71 <52495183+rohan-sr71@users.noreply.github.com>
2019-08-24 22:45:12 +0530

    Create .java

---
+++ .java
@@ -0,0 +1,54 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Hello 
+{
+	public static void main (String[] args)throws IOException
+	{
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		int n=Integer.parseInt(br.readLine());
+		while(n>0)
+		{
+		    n--;
+		    String str[]=br.readLine().trim().split("\\s+");
+		    String str1=Integer.toBinaryString(Integer.parseInt(str[0]));
+		    String str2=Integer.toBinaryString(Integer.parseInt(str[1]));
+		    int count=0;
+		    
+		    if(str1.length()>str2.length())
+		    {
+		        int x=str1.length()-str2.length();
+		        while(x!=0)
+		        {
+		            str2="0"+str2;
+		            x--;
+		        }
+		        
+		    }
+		    else if(str2.length()>str1.length())
+		    {
+		        int x=str2.length()-str1.length();
+		        while(x!=0)
+		        {
+		            str1="0"+str1;
+		            x--;
+		        }
+		        
+		    }
+		    //System.out.println(str1+" "+str2);
+		    for(int i=0;i<str1.length();i++)
+		    {
+		        if(str1.charAt(i)==(str2.charAt(i)))
+		        {
+		            
+		        }
+		        else
+		        {
+		            count++;
+		        }
+		    }
+		  
+		}
+	}
+}
