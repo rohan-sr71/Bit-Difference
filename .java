a473b4dd093bb31cdd90c081a24e5a8670500376
rohan-sr71 <52495183+rohan-sr71@users.noreply.github.com>
2020-02-09 08:11:53 +0530

    Update .java

--- .java
+++ .java
@@ -36,7 +36,7 @@ class Hello
 		        }
 		        
 		    }
-		    //System.out.println(str1+" "+str2);
+		    System.out.println(str1+" "+str2);
 		    for(int i=0;i<str1.length();i++)
 		    {
 		        if(str1.charAt(i)==(str2.charAt(i)))
